611c1bead93d19774053ba740c12b20d6a41b6ba
Mahmoud Abas <mmabas77@gmail.com>
2026-03-13 13:12:14 +0200

    fix: use inline styles on field type SVG icons (Tailwind classes not scanned in vendor)

--- resources/views/filament/fields/types.blade.php
+++ resources/views/filament/fields/types.blade.php
@@ -1,5 +1,5 @@
 <div class="py-2 cursor-pointer !w-full flex items-start justify-start gap-2">
-    @svg($field['icon'],'text-primary-500 w-5 h-5')
+    @svg($field['icon'], '', ['style' => 'width:1.25rem;height:1.25rem;flex-shrink:0;color:rgb(var(--primary-500))'])
     <span class="w-full flex items-center justify-between gap-2">
         <span class="flex flex-col items-start justify-between gap-2">
             <span class="font-semibold">{{ $field['title'] }}</span>
@@ -9,7 +9,7 @@
                 content: @js($field['description']),
                 theme: $store.theme,
             }">
-            @svg('heroicon-o-information-circle','mx-2 w-4 h-4 text-gray-400')
+            @svg('heroicon-o-information-circle', '', ['style' => 'width:1rem;height:1rem;flex-shrink:0;color:#9ca3af;margin:0 0.5rem'])
         </span>
     </span>
 </div>

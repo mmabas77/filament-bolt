9cfbe8ee2f542409ecf45d9e754bfe021206bae7
Mahmoud Abas <mmabas77@gmail.com>
2026-03-13 14:55:50 +0200

    feat(i18n): add Arabic translations for forms, collections, categories nav

---
+++ resources/lang/ar/category.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'label' => 'تصنيف',
+    'plural_label' => 'التصنيفات',
+    'navigation_label' => 'التصنيفات',
+    'name' => 'الاسم',
+    'slug' => 'الرابط',
+    'ordering' => 'الترتيب',
+    'is_active' => 'نشط',
+    'not_active' => 'غير نشط',
+    'description' => 'الوصف',
+    'logo' => 'الشعار',
+    'forms' => 'النماذج',
+];
